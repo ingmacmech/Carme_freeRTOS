59132b25298eb116819044d44c177eaa442f7154
ingmacmech <marcus.schluep@kns.ch>
2017-03-13 14:14:58 +0100

    add task status dummy text to LCDTask

--- EZBSY_U2A2/src/LCDTask.c
+++ EZBSY_U2A2/src/LCDTask.c
@@ -42,6 +42,9 @@
 //----- Data types -------------------------------------------------------------
 static const char* pcHello = "Hello freeRTOS";  /* Welcome text               */
 static const char* pcPotiText = "Speed = ";     /* Text to display value      */
+static const char* pcNumberOfTasks = "n Tasks Running = ";
+static const char* pcFreeStackSize = "Stack in use = ";
+static const char* pcHeapSize = "Remaining Heap Size = ";
 //----- Function prototypes ----------------------------------------------------
 
 //----- Data -------------------------------------------------------------------
@@ -65,6 +68,9 @@ void  vLCDTask(void *pvData) {
 
 	char    cBuffer[12];
 	uint16_t ledSpeed;					/* local copy of ledSpeed        	*/
+	uint16_t nTasks = 100;
+	uint16_t stackSize = 200;
+	uint16_t heapSize = 300;
 
 	/* Initialize the Display and display static text */
 	    LCD_Init();
@@ -72,6 +78,11 @@ void  vLCDTask(void *pvData) {
 	    LCD_DisplayStringCenterLine(Y_HEADERLINE, pcHello);
 	    LCD_SetFont(&font_8x13);
 	    LCD_DisplayStringXY(X_POTI_TEXT, Y_POTI, pcPotiText);
+	    LCD_DisplayStringXY(X_TASK_TEXT, Y_TASK, pcNumberOfTasks);
+	    LCD_DisplayStringXY(X_STACK_TEXT, Y_STACK, pcFreeStackSize);
+	    LCD_DisplayStringXY(X_HEAP_TEXT, Y_HEAP, pcHeapSize);
+
+
 
 
 	for (;;) {
@@ -85,6 +96,24 @@ void  vLCDTask(void *pvData) {
 		LCD_DisplayStringXY(X_POTI_VALUE, Y_POTI, "     ");
 		LCD_DisplayStringXY(X_POTI_VALUE, Y_POTI, cBuffer);
 
+		/* display number of tasks running */
+		sprintf(cBuffer, "%d", (int) nTasks);
+		LCD_DisplayStringXY(X_TASK_NUMBER, Y_TASK, "    ");
+		LCD_DisplayStringXY(X_TASK_NUMBER, Y_TASK, cBuffer);
         vTaskDelay(100 / portTICK_RATE_MS);
+
+        /* display stack size used*/
+        sprintf(cBuffer, "%d", (int) stackSize);
+        LCD_DisplayStringXY(X_STACK_NUMBER, Y_STACK, "     ");
+        LCD_DisplayStringXY(X_STACK_NUMBER, Y_STACK, cBuffer);
+
+        /* display heap size used */
+        sprintf(cBuffer, "%d", (int) heapSize);
+        LCD_DisplayStringXY(X_HEAP_NUMBER, Y_HEAP, "     ");
+        LCD_DisplayStringXY(X_HEAP_NUMBER, Y_HEAP, cBuffer);
+
+
+
+
     }
 }
--- EZBSY_U2A2/src/LCDTask.h
+++ EZBSY_U2A2/src/LCDTask.h
@@ -19,9 +19,22 @@
 
 //----- Macros -----------------------------------------------------------------
 #define Y_HEADERLINE    ( 1 )           /* pixel y-pos for headerline         */
+
 #define Y_POTI          ( 100 )         /* pixel y-pos for counter text       */
 #define X_POTI_TEXT     ( 16 )          /* pixel x-pos for counter text       */
 #define X_POTI_VALUE    ( 80 )          /* pixel x-pos for cycle value        */
+
+#define Y_TASK 		    ( 120 )         /* pixel y-pos for number of tasks    */
+#define X_TASK_TEXT     ( 16 )			/* pixel x-pos for text of tasks      */
+#define X_TASK_NUMBER	( 180 )			/* pixel x-pos for value 			  */
+
+#define Y_STACK 		( 140 )			/* pixel y-pos for stack size		  */
+#define X_STACK_TEXT    ( 16  )			/* pixel x-pos for stack text		  */
+#define X_STACK_NUMBER  ( 130 )			/* pixel x-pos for stack value		  */
+
+#define Y_HEAP			( 160 )			/* pixel y-pos for heap size		  */
+#define X_HEAP_TEXT		( 16 )			/* pixel x-pos for heap text		  */
+#define X_HEAP_NUMBER   ( 200 )			/* pixel x-pos for heap value		  */
 
 //----- Data types -------------------------------------------------------------
 
